18eaaf65d77076b49023e4e50bfeff86339eb7b9
idest <echeverriajavier@gmail.com>
2017-09-02 16:24:55 -0300

    Workin on compute.py, Geometric Model done, Thermal Model in progress

--- misc/test.py
+++ misc/test.py
@@ -9,23 +9,310 @@ from pyevtk.hl import imageToVTK
 
 #data = np.loadtxt('/Users/inigo/projects/andestm-web/media/data/Modelo.dat')
 data = np.loadtxt('/home/idest/projects/andestm/media/data/Modelo.dat')
+ta = np.loadtxt('/home/idest/projects/andestm/media/data/PuntosFosaEdad.dat')
 
+class Data(object):
+    def __init__(self,data,xystep,zstep):
+        self.data = data
+        self.xystep = xystep
+        self.zstep = zstep
+        self.xaxis = self.__setAxis(values=self.data[:, 0])
+        self.yaxis = self.__setAxis(values=self.data[:, 1], reversed=True)
+        self.zaxis = self.__setAxis(values=self.data[:, 5], reversed=True,
+                                    lowvalues=self.data[:, 2], zaxis=True)
+        self.mask2D = self.__set2DMask()
+        self.mask3D = self.__set3DMask()
+        self.grid2D = self.__setGrid([self.xaxis, self.yaxis],
+                                     mask=True)
+        self.grid3D = self.__setGrid([self.xaxis, self.yaxis, self.zaxis],
+                                     mask=True)
+    def __setAxis(self, values, lowvalues=False, reversed=False, zaxis=False):
+        if lowvalues is False:
+            lowvalues = values
+        maxv = math.ceil(np.nanmax(values))
+        minv = math.floor(np.nanmin(lowvalues))
+        if reversed == True:
+            firstv, lastv = maxv, minv
+        else:
+            firstv, lastv = minv, maxv
+        if zaxis == True:
+            step = self.zstep
+        else:
+            step = self.xystep
+        return np.linspace(firstv, lastv,
+                           num=(abs(lastv-firstv))/step+1,
+                           endpoint=True)
+    def __set2DMask(self):
+        # Use lab/slab data nan values as mask
+        return np.isnan(self.reshape2DData(self.data[:, 2]))
+    def __set3DMask(self):
+        # Use lab/slab data nan values as mask
+        mask2D = self.mask2D
+        mask3D = np.zeros(self.get3DShape(), dtype=bool)
+        mask3D[:, :, :] = mask2D[:, :, np.newaxis]
+        return mask3D
+    def __setGrid(self, axes, mask=False):
+        grid = np.meshgrid(*[n for n in axes], indexing='ij')
+        maskedgrid = []
+        if mask is True:
+            for n in grid:
+                if len(n.shape) == 2:
+                    maskedgrid.append(self.mask2DArray(n, nanfill=True))
+                else:
+                    maskedgrid.append(self.mask3DArray(n, nanfill=True))
+            return maskedgrid
+        else:
+            return grid
+    def getData(self):
+        return self.data
+    def getAxes(self):
+        return [self.xaxis, self.yaxis, self.zaxis]
+    def get2DShape(self):
+        return (len(self.xaxis), len(self.yaxis))
+    def get3DShape(self):
+        return (len(self.xaxis), len(self.yaxis), len(self.zaxis))
+    def get2DGrid(self):
+        return self.grid2D
+    def get3DGrid(self):
+        return self.grid3D
+    def get2DIndexes(self):
+        return np.indices(self.get2DShape())
+    def get3DIndexes(self):
+        return np.indices(self.get3DShape())
+    def reshape2DData(self, data2D):
+        return data2D.T.reshape((len(self.yaxis), len(self.xaxis))).T
+    def mask2DArray(self, array2D, nanfill=False):
+        mask2D = np.ma.array(array2D, mask=self.mask2D)
+        if nanfill is True:
+            mask2D = mask2D.filled(np.nan)
+        return mask2D
+    def mask3DArray(self, array3D, nanfill=False):
+        mask3D = np.ma.array(array3D, mask=self.mask3D)
+        if nanfill is True:
+            mask3D = mask3D.filled(np.nan)
+        return mask3D
+    def delimitArea(self):
+        rows = np.repeat(np.arange(len(idxs)), idxs)
+        cols = np.ones(idxs.sum(), dtype=int)
+        cols[np.cumsum(idxs)[:-1]] -= idxs[:-1]
+        cols = np.cumsum(cols) - 1
+        areas = np.ones((idxs.shape[0], self.grid2D[2].shape[1]))
+        areas[rows, cols] = 0
+        return areas
+    def printdata(self, name, data, bool=False):
+        if bool is True:
+            np.savetxt(name, data, fmt="%1d", delimiter=" ")
+        else:
+            np.savetxt(name, data, fmt="%9.2e", delimiter="   ")
+
+class Model3D(object):
+    def __init__(self):
+        pass
+
+class GeometricModel(Model3D):
+    def __init__(self, data):
+        self.data = data
+        self.z_sl = self.__setBoundary(self.data.getData()[:, 2])
+        self.z_moho = self.__setBoundary(self.data.getData()[:, 3])
+        self.z_icd = self.__setBoundary(self.data.getData()[:, 4])
+        self.z_topo = self.__setBoundary(self.data.getData()[:, 5])
+        self.geomodel3D = self.__set3DGeoModel()
+        self.slablabdepth = self.__setSlabLabDepth()
+        self.slablabarea = self.__setSlabLabArea()
+        #self.GModel3D = self.__set3DGModel()
+    def __setBoundary(self, depthData):
+        return self.data.reshape2DData(depthData)
+    def __set3DGeoModel(self):
+        boundaries = [self.z_topo, self.z_icd, self.z_moho, self.z_sl]
+        z = self.data.get3DGrid()[2]
+        geomodel3D = self.data.mask3DArray(np.empty(z.shape)*np.nan,
+                                           nanfill=True)
+        for n in range(len(boundaries)):
+            c = boundaries[n][:, :, np.newaxis]
+            with np.errstate(invalid='ignore'): ### ?? ###
+                geomodel3D[z < c] = n + 1
+        return geomodel3D
+    def __setLayerThickness(self):
+        pass
+    def __getSlabLabIndex(self):
+        # Gradients array
+        g = np.gradient(self.z_sl, axis=0)
+        # Min Gradient Indexes
+        mingidx = np.nanargmin(np.ma.masked_invalid(g), axis=0)
+        # Positive gradient boolean array (True where gradient > 0 ...)
+        with np.errstate(invalid='ignore'):
+            posg = g > -4e-01 # (... or where gradient gets very close to 0)
+        # Ignore (mask) the values to the left of the minimum gradient
+        gmask = self.data.get2DIndexes()[0] < mingidx
+        maskedposg = np.ma.array(posg, mask=gmask)
+        # Get index of first positive gradient (Slab/Lab idx)
+        slablabidx = np.argmax(maskedposg, axis=0)
+        # Get visual boolean representation of slablabidx
+        # Z = np.zeros(g.shape)
+        # Z[slablabidx, self.data.get2DIndexes()[1][0]] = 1
+        # return Z.T
+        return slablabidx
+    def __setSlabLabDepth(self):
+        jidxs = self.data.get2DIndexes()[1][0]
+        slablabdepth = self.z_sl[self.__getSlabLabIndex(), jidxs]
+        return slablabdepth
+    def __setSlabLabArea(self):
+        slablabarea = np.zeros(self.data.get2DShape())
+        slablabarea[self.data.get2DIndexes()[0] > self.__getSlabLabIndex()] = 1
+        return slablabarea
+    def get3DGeoModel(self):
+        return self.geomodel3D
+    def getBoundaries(self):
+        return [self.z_topo, self.z_icd, self.z_moho, self.z_sl]
+    def getSlabLabDepth(self):
+        return self.slablabdepth
+    def getSlabLabArea(self):
+        return self.slablabarea
+
+A = Data(data,0.2,1)
+#A.printdata("masked_data")
+#A1 = A.get3DGrid()[0]
+#print(A1.shape)
+#A2 = np.swapaxes(A1,0,1)
+#print(A2.shape)
+#A.printdata('topslicex3', A2[:,:,0])
+B = GeometricModel(A)
+C = B.get3DGeoModel()
+#A.printdata('slablabarea_4e01sa', B.getSlabLabArea().T, bool=True)
+
+class ThermalModel(Model3D):
+    def __init__(self, geomodel, Tdata):
+        self.geomodel = geomodel
+        self.tdata = Tdata
+        self.K = __setLayerProperty("K")
+        self.H = __setLayerProperty("H")
+    def __setLayerProperty(self, prop):
+        geomodel3D = self.geomodel.get3DGeoModel()
+        boundaries = self.geomodel.getBoundaries()
+        if prop == "K":
+            propv = [self.tdata.K_cs, self.tdata.K_ci, self.tdata.K_ml]
+            propfz = self.tdata.K_z
+        elif prop == "H":
+            propv = [self.tdata.H_cs, self.tdata.H_ci, self.tdata.H_ml]
+            propfz = self.tdata.H_z
+        if propfz is True:
+            R = np.empty(geomodel3D.shape)*np.nan
+            R[geomodel3D == 1] = propv[0]
+            R[geomodel3D == 2] = propv[1]
+            R[geomodel3D == 3] = propv[2]
+        else:
+            Rh = []
+            h = [] #h = thickness
+            while n < len(boundaries)-1:
+                h.append(boundaries[n] - boundaries[n+1])
+                Rh.append(propv[n]*h[n])
+            R = (Rh.sum()/h.sum())[:,:,np.newaxis]
+        return R
+    def __setDelta(self):
+        ### Work needs to be done here ###
+        boundaries = self.geomodel.getBoundaries()
+        if self.tdata.d_rad == 0:
+            delta = boundaries[0] - boundaries[1]
+        else:
+            delta = d_rad
+    def __getTrenchAge(self):
+        if EF_lat is True:
+            trenchage = ta[1]
+        else:
+            trenchage = self.tdata.EF
+    def __getQZero(self):
+        qzero = (self.K * self.tdata.T_p)/np.sqrt(np.pi * self.tdata.kpa *
+                                                  self.__getTrenchAge())
+        return qzero
+    def __getS(self):
+        slablabdepth = self.geomodel.getSlabLabDepth()
+        topo = self.geomodel.getBoundaries()[0]
+        s = 1 + self.tdata.b * np.sqrt(slablabdepth - topo) * self.tdata.V *
+            abs(np.sin(self.tdata.dip) / self.tdata.kpa)
+        return s
+    def __getSlabLabTemp(self):
+        slablabdepth = self.geomodel.getSlabLabDepth()
+        slablabtemp = self.tdata.T_p + self.tdata.G_a * slablabdepth
+        return slablabtemp
+    def __getSigmaMax(self):
+        slablabtemp = self.__getSlabLabTemp()
+        slablabdepth = self.geomodel.getSlabLabDepth()
+        topo = self.geomodel.getBoundaries()[0]
+        sigmamax = (slablabtemp / (slablabdepth - slablabtemp) /
+                   (self.tdata.V * self.tdata.__getS() * self.K) -
+                   self.__getQZero() * self.tdata.kpa)
+        return sigmamax
+    def __getMu(self):
+        mu = self.__getSigmaMax() / 1 - np.exp(self.tdata.D2)
+        return mu
+    def __getSigma(self):
+        mu = self.__getMu()
+        slablab = self.geomodel.getBoundaries()[3]
+        topo = self.geomodel.getBoundaries()[0]
+        slablabdepth = self.geomodel.getSlabLabDepth()
+        sigma = mu * (1 - np.exp((slablab - topo)*self.tdata.D2 /
+                      (slablabdepth - topo)))
+        return sigma
+    def __getSlabTemp(self):
+        qzero = self.__getQZero()
+        sigma = self.__getSigma()
+        labslab = self.geomodel.getBoundaries()[3]
+        topo = self.geomodel.getBoundaries()[0]
+        s = self.__getS()
+        K = self.K
+        slabtemp = (qzero + sigma * self.tdata.V) * (labslab - topo) / (s * K)
+        return slabtemp
+    def __getLabTemp(self):
+        labslab = self.geomodel.getBoundaries()[3]
+        labtemp = self.tdata.T_p + self.tdata.G_a * labslab
+        return labtemp
+        ###WORk IN PROGRESS###
+
+
+
+# for n in np.arange(1000, 4200, 20):
+#     if n == 1000:
+#         file = np.loadtxt('perfs-old/perf-' + str(n))[:, 6][:, np.newaxis]
+#     else:
+#         file2 = np.loadtxt('perfs-old/perf-' + str(n))[:, 6][:, np.newaxis]
+#         file = np.append(file, file2, axis=1)
+# #print(file.shape)
+# #a = file == 0
+# #print(a.shape)
+# #print(a)
+# file[file == 0] = 1
+# file[file == 1] = 0
+# file[file == 2] = 1
+# A.printdata('oldareas', file.T, bool=True)
+
+#C2 = np.swapaxes(B.get3DGeoModel(), 0,1)
+#print(A.get3DGrid()[2].shape)
+#C = A.printdata('topslicez', A.get3DGrid()[2][:,:,0])
+#D = A.get3DGrid()[2].filled(np.nan)
+
+#imageToVTK("./block", pointData = {"elevation": D})
+
+#rndm = np.sin(A.get3DGrid()[0]) + np.sin(A.get3DGrid()[1]) + np.sin(A.get3DGrid()[2]/5)*A.get3DGrid()[2]/100
+#rndm = rndm.filled(np.nan)
+imageToVTK("./block", pointData = {"elevation": C})
+
+"""
 class DataGrid(object):
     def __init__(self, data):
-        """
-        self.col = int(self.ceil - self.floor + 1) #Maximo rango de profundidades
-        """
+
+        #self.col = int(self.ceil - self.floor + 1) #Maximo rango de profundidades
+
         self.xystep = 0.2
         self.zstep = 1
 
         # X Axis
         minlon = math.floor(np.nanmin(data[:, 0]))
-        maxlon = math.ceil(np.nanmax(data[:, 0]))
+        maxlon = math.ceil(np.nanmax(daNew Bookmarkta[:, 0]))
         self.xaxis = np.linspace(minlon, maxlon,
                                  num=(maxlon-minlon)/self.xystep+1,
                                  endpoint=True)
         # Y Axis
-        minlat = math.floor(np.nanmin(data[:, 1]))
+        minlat = math.floor(np.nanmin(daNew Bookmarkta[:, 1]))
         maxlat = math.ceil(np.nanmax(data[:, 1]))
         self.yaxis = np.linspace(maxlat, minlat,
                                  num=(abs(minlat-maxlat))/self.xystep+1,
@@ -48,7 +335,7 @@ class DataGrid(object):
 
 class LitLayer(object):
     def __init__(self, datacol, grid):
-        """data consist of 3 columns, x, y, and layer's depth"""
+        #data consist of 3 columns, x, y, and layer's depth
         self.Z = datacol.T.reshape(grid.X.shape)
     def delimit(self, idxs):
         rows = np.repeat(np.arange(len(idxs)), idxs)
@@ -129,3 +416,4 @@ LABSLAB3D = np.dstack([TOPO.Z]*1)
 rndm = np.sin(GRID.XX) + np.sin(GRID.YY) + np.sin((GRID.ZZ)/5)*GRID.ZZ/100
 print(rndm.shape)
 imageToVTK("./block", pointData = {"elevation": rndm})
+"""
--- misc/vtkRoutine.py
+++ misc/vtkRoutine.py
@@ -1,5 +1,9 @@
 import vtk
 
+###############################################################################
+################################### Readers ###################################
+###############################################################################
+
 reader = vtk.vtkXMLImageDataReader()
 reader.SetFileName("block.vti")
 reader.Update()
@@ -7,15 +11,16 @@ reader.Update()
 print reader.GetOutput()
 array = reader.GetOutput().GetPointData().GetArray(0) #.GetRange()
 reader.GetOutput().GetPointData().SetScalars(array)
+print reader.GetOutput().GetPointData().GetScalars()
 #print reader.GetOutput().GetClassName()
 #print reader.GetOutput().GetPointData().GetScalars()
 #print reader.GetOutput().GetScalarRange()
 print reader.GetOutput().GetBounds()
-reader.GetOutput().SetOrigin(0,0,-178)
+reader.GetOutput().SetOrigin(0,0,0)
 #reader.GetOutput().SetOrigin(0,0,-178)
 #reader.GetOutput().SetDimensions(176,101,1)
 #reader.GetOutput().AllocateScalars(vtk.VTK_DOUBLE, 1)
-
+"""
 labSlabReader = vtk.vtkXMLImageDataReader()
 labSlabReader.SetFileName("labslab.vti")
 labSlabReader.Update()
@@ -39,11 +44,15 @@ topoReader.SetFileName("topo.vti")
 topoReader.Update()
 topoArray = topoReader.GetOutput().GetPointData().GetArray(0)
 topoReader.GetOutput().GetPointData().SetScalars(topoArray)
+"""
+###############################################################################
+################################### Filters ###################################
+###############################################################################
 
-#imageDataGeometryFilter = vtk.vtkImageDataGeometryFilter()
-#imageDataGeometryFilter.SetInputConnection(reader.GetOutputPort())
-#imageDataGeometryFilter.Update()
-
+geometryFilter = vtk.vtkImageDataGeometryFilter()
+geometryFilter.SetInputConnection(reader.GetOutputPort())
+geometryFilter.Update()
+"""
 labSlabTriangles = vtk.vtkDataSetTriangleFilter()
 labSlabTriangles.SetInputConnection(labSlabReader.GetOutputPort())
 labSlabTriangles.Update()
@@ -76,9 +85,9 @@ icdWarp.Update()
 topoWarp = vtk.vtkWarpScalar()
 topoWarp.SetInputConnection(topoTriangles.GetOutputPort())
 topoWarp.Update()
-
+"""
 plane = vtk.vtkPlane()
-plane.SetOrigin(50,50,50)
+plane.SetOrigin(0,0,100)
 plane.SetNormal(0,0,1)
 
 cutter = vtk.vtkCutter()
@@ -86,14 +95,30 @@ cutter.SetCutFunction(plane)
 cutter.SetInputConnection(reader.GetOutputPort())
 cutter.Update()
 
+extractVOI = vtk.vtkExtractVOI()
+extractVOI.SetInputData(reader.GetOutput())
+extractVOI.SetVOI(0,100,0,175,20,20)
+extractVOI.GetOutput().GetPointData().SetScalars(array)
+print "voi", extractVOI.GetOutput().GetPointData().GetScalars()
+#extractVOI.GetOutput().GetPointData().SetScalars(array)
+extractVOI.Update()
+
+###############################################################################
+################################### Mappers ###################################
+###############################################################################
+
 mapper = vtk.vtkDataSetMapper()
 #mapper = vtk.vtkPolyDataMapper()
 a, b = reader.GetOutput().GetPointData().GetArray(0).GetRange()
 mapper.SetScalarRange(a, b)
 #mapper.ScalarVisibilityOn()
+#mapper.SetInputConnection(geometryFilter.GetOutputPort())
 mapper.SetInputConnection(reader.GetOutputPort())
 #mapper.SetInputConnection(imageDataGeometryFilter.GetOutputPort())
-#nifdagnjafkdgnjdfsz;p
+lut = mapper.GetLookupTable()
+#lut.SetNanColor(0,0,0,0)
+#mapper.SetLookupTable(lut)
+"""
 labSlabMapper = vtk.vtkDataSetMapper()
 labSlabMapper.SetInputConnection(labSlabWarp.GetOutputPort())
 labSlabMapper.ScalarVisibilityOff()
@@ -109,75 +134,133 @@ icdMapper.ScalarVisibilityOff()
 topoMapper = vtk.vtkDataSetMapper()
 topoMapper.SetInputConnection(topoWarp.GetOutputPort())
 topoMapper.ScalarVisibilityOff()
-
+"""
 sliceMapper = vtk.vtkDataSetMapper()
+sliceMapper.SetInputConnection(cutter.GetOutputPort())
+sliceMapper.SetScalarRange(a, b)
+
+"""
+sliceMapper = vtk.vtkPolyDataMapper2D()
 a, b = reader.GetOutput().GetPointData().GetArray(0).GetRange()
 sliceMapper.SetScalarRange(a, b)
-sliceMapper.SetInputConnection(cutter.GetOutputPort())
+print cutter.GetOutput().GetClassName()
+sliceMapper.SetInputData(cutter.GetOutput())
+"""
+###############################################################################
+################################### Actors ####################################
+###############################################################################
 
 actor = vtk.vtkActor()
 actor.SetMapper(mapper)
-actor.VisibilityOff()
-#context = actor.GetProperty()
-#context.SetRepresentationToWireframe()
-
+#actor.VisibilityOff()
+context = actor.GetProperty()
+context.SetRepresentationToWireframe()
+"""
 labSlabActor = vtk.vtkActor()
 labSlabActor.SetMapper(labSlabMapper)
 labSlabContext = labSlabActor.GetProperty()
 labSlabContext.SetDiffuseColor(1, 0, 0)
+labSlabActor.VisibilityOff()
 
 mohoActor = vtk.vtkActor()
 mohoActor.SetMapper(mohoMapper)
 mohoContext = mohoActor.GetProperty()
 mohoContext.SetDiffuseColor(0, 1, 0)
+mohoActor.VisibilityOff()
 
 icdActor = vtk.vtkActor()
 icdActor.SetMapper(icdMapper)
 icdContext = icdActor.GetProperty()
 icdContext.SetDiffuseColor(0, 0, 1)
+icdActor.VisibilityOff()
 
 topoActor = vtk.vtkActor()
 topoActor.SetMapper(topoMapper)
 topoContext = topoActor.GetProperty()
 topoContext.SetDiffuseColor(1, 1, 0)
+topoActor.VisibilityOff()
+"""
 
 sliceActor = vtk.vtkActor()
 sliceActor.SetMapper(sliceMapper)
+"""
+sliceActor = vtk.vtkImageActor()
+sliceActor.GetMapper().SetInputData(extractVOI.GetOutput())
+#sliceActor.GetMapper().SetScalarRange(a, b)
+sliceActor.GetProperty().SetLookupTable(lut)
+print sliceActor.GetProperty().GetColorLevel()
+#sliceActor.GetProperty().UseLookupTableScalarRangeOn()
+#sliceActor.InterpolateOn()
+#sliceActor.SetOpacity(50)
+#sliceActor.SetColor(1,1,0)
+"""
+"""
+sliceActor = vtk.vtkActor2D()
+#sliceActor.GetProperty().SetColor(1, 1, 0)
+#sliceActor.GetProperty().SetEdgeColor(0, 1, 0)
+#sliceActor.GetProperty().SetLineWidth(2)
+#sliceActor.GetProperty().EdgeVisibilityOn()
+sliceActor.SetMapper(sliceMapper)
+print "coord", sliceActor.GetPositionCoordinate()
+#sliceActor.VisibilityOff()
+"""
 
 ## Display axes
-#transform = vtk.vtkTransform()
-#transform.Translate(1.0, 0.0, 0.0)
-#axes = vtk.vtkAxesActor
-#axes.SetUserTransform(transform)
+transform = vtk.vtkTransform()
+transform.Translate(1.0, 0.0, 0.0)
+axes = vtk.vtkAxesActor()
+axes.SetTotalLength(20,20,20)
+#print axes.SetXAxisLabelText()
+axes.GetXAxisCaptionActor2D().GetCaptionTextProperty().SetFontSize(25)
+#print axes.GetXAxisCaptionActor2D().GetCaptionTextProperty().SetColor(1,1,0)
+axes.GetXAxisCaptionActor2D().GetTextActor().SetTextScaleMode(0)
+axes.GetYAxisCaptionActor2D().GetCaptionTextProperty().SetFontSize(25)
+axes.GetYAxisCaptionActor2D().GetTextActor().SetTextScaleMode(0)
+axes.GetZAxisCaptionActor2D().GetCaptionTextProperty().SetFontSize(25)
+axes.GetZAxisCaptionActor2D().GetTextActor().SetTextScaleMode(0)
+axes.SetUserTransform(transform)
+
+###############################################################################
+################################## Rendering ##################################
+###############################################################################
 
 renderer = vtk.vtkRenderer()
 renderer.AddActor(actor)
+"""
 renderer.AddActor(labSlabActor)
 renderer.AddActor(mohoActor)
 renderer.AddActor(icdActor)
 renderer.AddActor(topoActor)
-#renderer.AddActor(axes)
+"""
+renderer.AddActor(axes)
 renderer.SetBackground(0,0,0)
-#renderer.SetViewport(0,0,0.5,1)
+renderer.SetViewport(0,0,0.5,1)
 renderer.ResetCamera()
 
 sliceRenderer = vtk.vtkRenderer()
 sliceRenderer.AddActor(sliceActor)
+#sliceRenderer.AddActor(axes)
 sliceRenderer.SetBackground(0,0,0)
 sliceRenderer.SetViewport(0.5,0,1,1)
-sliceRenderer.ResetCamera()
+#sliceRenderer.ResetCamera()
 
 renwin = vtk.vtkRenderWindow()
 renwin.AddRenderer(renderer)
-#renwin.AddRenderer(sliceRenderer)
+renwin.AddRenderer(sliceRenderer)
 renwin.SetSize(1600,800)
+
+###############################################################################
+################################# Interactor ##################################
+###############################################################################
 
 iren = vtk.vtkRenderWindowInteractor()
 iren.SetRenderWindow(renwin)
 
 widget = vtk.vtkImplicitPlaneWidget()
 print cutter.GetOutput().GetBounds()
-widget.PlaceWidget(cutter.GetOutput().GetBounds())
+print reader.GetOutput().GetBounds()
+print "bounds", reader.GetOutput().GetBounds()
+widget.PlaceWidget((-100.0, 200.0, -100.0, 275.0, -100.0, 283.0))
 widget.SetOrigin([plane.GetOrigin()[x] for x in 0,1,2])
 widget.SetNormal([plane.GetNormal()[x] for x in 0,1,2])
 
@@ -186,12 +269,14 @@ widget.SetInteractor(iren)
 def eventhandler(obj,event):
     global plane
     obj.GetPlane(plane)
+    #sliceRenderer.ResetCamera()
 widget.AddObserver("InteractionEvent", eventhandler)
 
-widget.SetEnabled(0)
-#widget.DrawPlaneOn()
-#widget.TubingOn()
+widget.SetEnabled(1)
+widget.DrawPlaneOn()
+widget.TubingOn()
 
 iren.Initialize()
 renwin.Render()
+
 iren.Start()
